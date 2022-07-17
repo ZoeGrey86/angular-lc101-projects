01a349a59d2264c48a445278628eedb53f805805
Zoe Smith <107585629+ZoeGrey86@users.noreply.github.com>
2022-07-16 22:21:57 -0500

    at the point where it says to commit

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Tribute Photos';
+  image1 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR60mcOjhEJW4y4hSiKLu6K_fTPBEBAB-eYOA&usqp=CAU';
+  image2 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSide6ya6m5W2U-bDyB6vSSFFkAvvh7n-uw5g&usqp=CAU';
+  image3 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_wXnGpAr4RVw7AWtNyLw1jGgcLuN-1DI_2Q&usqp=CAU';
 
   constructor() { }
 
--- lesson1/exercises/src/app/movie-list/movie-list.component.ts
+++ lesson1/exercises/src/app/movie-list/movie-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-   movies = ['The Manchurian Candidate', 'Oceans 8'];
+   movies = ['The Manchurian Candidate', 'Oceans 8', "O'Brother Where art thou", "But I'm a Cheerleader", "10 Things I hate about you","Knights Tale"];
 
    constructor() { }
 
